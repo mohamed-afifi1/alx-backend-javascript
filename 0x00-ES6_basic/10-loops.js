cb176e63256d789caa2bd753a9e0b01ca23c696f
root <mohammed2004koki@gmail.com>
2024-09-30 22:56:41 +0300

    10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let idx of array) {
-    idx += appendString;
+  for (const value of array) {
+    const idx = array.indexOf(value);
+    array[idx] = appendString + value; // eslint-disable-line no-param-reassign
   }
 
   return array;
