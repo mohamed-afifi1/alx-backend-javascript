c31573ea818e8eaf26b1b17858a732623073c722
mohamed-afifi1 <mohammed2004koki@gmail.com>
2024-11-28 19:38:56 +0200

    test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,17 +4,22 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', function () {
     it('test1', function () {
         assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.strictEqual(calculateNumber(-1.5, -3.5), -4);
     });
     it('test2', function () {
       assert.strictEqual(calculateNumber(1, 3.7), 5);
+      assert.strictEqual(calculateNumber(-1, -3.7), -5);
     });
     it('test3', function () {
       assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+      assert.strictEqual(calculateNumber(-1.2, -3.7), -5);
     });
     it('test4', function () {
       assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+      assert.strictEqual(calculateNumber(-1.5, -3.7), -5);
     });
     it('test5', function () {
       assert.strictEqual(calculateNumber(1.5, 3), 5);
+      assert.strictEqual(calculateNumber(-1.5, -3), -4);
     });
 });
