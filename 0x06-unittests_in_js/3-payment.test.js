5cad236f2f300f78e4777278a9848fd4679aeff1
mohamed-afifi1 <mohammed2004koki@gmail.com>
2024-11-28 19:17:29 +0200

    3-tests

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,25 @@
+/**
+ * Test that payment called Utils.calculateNumber
+ */
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  beforeEach(function() {
+    sinon.spy(Utils, 'calculateNumber');
+  });
+  afterEach(function() {
+    sinon.restore();
+  });
+  it("tests", function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(Utils.calculateNumber.called).to.be.true;
+    expect(Utils.calculateNumber.calledOnce).to.be.true;
+    expect(Utils.calculateNumber.callCount).to.be.equal(1);
+    expect(Utils.calculateNumber.firstCall.args[0]).to.equal("SUM");
+    expect(Utils.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+  });
+});
